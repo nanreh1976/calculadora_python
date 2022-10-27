90de4fbd8b4997c0b5a39dffe4932c4546558158
Hernan Gomez <95485080+nanreh1976@users.noreply.github.com>
2022-10-27 12:04:00 -0300

    calculadora basica de python

---
+++ main.py
@@ -0,0 +1,14 @@
+from calculos import suma, resta, division, multiplicacion
+
+print("-"*50)
+var1=int(input("ingresa el primer valor : "))
+print("-"*50)
+var2=int(input("ingrese el segundo valor : "))
+print("-"*50)
+print("la suma de ",var1," y ",var2," es  : ",suma(var1,var2))
+print("-"*50)
+print("la resta de ",var1," y ",var2," es  : ",resta(var1,var2))
+print("-"*50)
+print("la multiplicacion de ",var1," y ",var2," es  : ",multiplicacion(var1,var2))
+print("-"*50)
+print("la division de ",var1," y ",var2," es  : ",division(var1,var2))
